da4a23e6d65b3571b7a9ad6aa8c109e64578b563
ProgrammerPat <programmerpatrick123@gmail.com>
2019-04-13 10:00:33 -0700

    commit7

--- js/script.js
+++ js/script.js
@@ -21,41 +21,23 @@ const list = document.getElementsByClassName('student-item');
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+
+
+//dont display the whole list or delete parts. Just display the listItems chosen.
 
 function showPage(list, page){
     
-    var listItems=[];
-
-    for(var i = 0; i<(10 || listItems.length); i++){
-        listItems.push(list[i+10*page]);
+    for (var i = 0; i <list.length; i++){
+        list[i].style.display="none";
     }
     
-    for(var i = list.length; i > 0; i--){
-        list[i-1].remove();
+   for(var i=0+page*10; i<10+page*10; i++){
+       list[i].style.display="block";
     }
-    
-    for(var i = 0; i<listItems.length; i++){
-    
-    document.querySelector('ul.student-list').appendChild(listItems[i]);
-    
-    }
+  
     
 }
-
 
 
 
@@ -75,10 +57,16 @@ function appendPageLinks(list) {
         a.innerHTML = i+1;
     }
     document.querySelector("div.page").appendChild(div);
+    
+
 }
+
+//one more function that controls the page links with event listeners
+
 
 appendPageLinks(list);
 
+showPage(list,2);
 
-showPage(list,1);
+
 
